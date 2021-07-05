c7d15e144f0a84d9ed636f0abfed159c64725758
allhail-thanatos <80190865+allhail-thanatos@users.noreply.github.com>
2021-07-05 16:12:35 +0700

    Add files via upload

---
+++ pppll/application/controllers/cregister.php
@@ -0,0 +1,136 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+class cregister extends CI_Controller {
+	
+	public function __construct()
+	{
+		parent::__construct();
+		$this->load->model('mregister');
+	}
+ 
+	// Index login
+	public function index() {
+        $this->form_validation->set_rules('nama_user', 'Nama', 'trim|required');
+        $this->form_validation->set_rules('pass_user', 'Password', 'trim|required');
+        $this->form_validation->set_rules('jk_user', 'Jenis Kelamin', 'trim|required');
+        $this->form_validation->set_rules('alamat_user', 'Alamat', 'trim|required');
+        $this->form_validation->set_rules('telp_user', 'No Telepon', 'trim|required');
+        $this->form_validation->set_rules('email_user', 'Email', 'trim|required');
+        $this->form_validation->set_message('required', '*) Lengkapi Data Anda!');
+
+		if ($this->form_validation->run() == FALSE)
+        {    
+    		$data='';
+
+            // echo "<pre>";
+            // print_r($data);
+            // echo "<pre>";
+            // exit();
+            $this->template->load('admin', 'content' , 'layout/regis',$data);
+        }
+        else
+        {       
+            $this->id = uniqid();
+            $data['nama_user']		= $this->input->post('nama_user');
+            $data['pass_user']		= $this->input->post('pass_user');
+			$data['jk_user']		= $this->input->post('jk_user');
+			$data['alamat_user']	= $this->input->post('alamat_user');
+			$data['telp_user']		= $this->input->post('telp_user');
+			$data['email_user']		= $this->input->post('email_user');
+            $data['role_user']      = 'Pelanggan';
+
+            $id_user = $this->mregister->register($data);
+
+            $config['upload_path']          = '../upload/foto_profile/';
+            $config['allowed_types']        = 'jpg|jpeg|png';
+            $config['file_name']            = $this->id.'-'.$id_user.'-'.$this->input->post('nama_user');
+            // $config['encrypt_name']      = true;
+            $config['overwrite']            = true;
+            $config['max_size']             = 2048; // 1MB
+            // $config['max_width']            = 1024;
+            // $config['max_height']           = 768;
+
+            $this->load->library('upload', $config);
+
+            if (!empty($_FILES["foto_user"]["name"])) {
+              if ($this->upload->do_upload('foto_user')) {
+                $data1['foto_user'] = $this->upload->data('file_name');
+                }
+            } else {
+              $data1['foto_user'] = $this->input->post('old_foto');
+            }
+
+			// echo "<pre>";
+			// print_r($data);
+   //          print_r($data1);
+			// echo "<pre>";
+			// exit();
+
+            $this->mregister->update_user($data1, $id_user);
+        }
+
+	}
+
+	public function login() {
+		$this->form_validation->set_rules('nama_user', 'Nama', 'trim|required');
+        $this->form_validation->set_rules('pass_user', 'Password', 'trim|required');
+        $this->form_validation->set_rules('jk_user', 'Jenis Kelamin', 'trim|required');
+        $this->form_validation->set_rules('alamat_user', 'Alamat', 'trim|required');
+        $this->form_validation->set_rules('telp_user', 'No Telepon', 'trim|required');
+        $this->form_validation->set_rules('email_user', 'Email', 'trim|required');
+        $this->form_validation->set_message('required', '*) Lengkapi Data Anda!');
+
+		if ($this->form_validation->run() == FALSE)
+        {    
+    		$data='';
+
+            // echo "<pre>";
+            // print_r($data);
+            // echo "<pre>";
+            // exit();
+            $this->template->load('admin', 'content' , 'layout/regis',$data);
+        }
+        else
+        {       
+            $this->id = uniqid();
+            $data['nama_user']		= $this->input->post('nama_user');
+            $data['pass_user']		= $this->input->post('pass_user');
+			$data['jk_user']		= $this->input->post('jk_user');
+			$data['alamat_user']	= $this->input->post('alamat_user');
+			$data['telp_user']		= $this->input->post('telp_user');
+			$data['email_user']		= $this->input->post('email_user');
+            $data['role_user']      = 'Pelanggan';
+
+            $id_user = $this->mregister->register($data);
+
+            $config['upload_path']          = '../upload/foto_profile/';
+            $config['allowed_types']        = 'jpg|jpeg|png';
+            $config['file_name']            = $this->id.'-'.$id_user.'-'.$this->input->post('nama_user');
+            // $config['encrypt_name']      = true;
+            $config['overwrite']            = true;
+            $config['max_size']             = 2048; // 1MB
+            // $config['max_width']            = 1024;
+            // $config['max_height']           = 768;
+
+            $this->load->library('upload', $config);
+
+            if (!empty($_FILES["foto_user"]["name"])) {
+              if ($this->upload->do_upload('foto_user')) {
+                $data1['foto_user'] = $this->upload->data('file_name');
+                }
+            } else {
+              $data1['foto_user'] = $this->input->post('old_foto');
+            }
+
+			// echo "<pre>";
+			// print_r($data);
+   //          print_r($data1);
+			// echo "<pre>";
+			// exit();
+
+            $this->mregister->update_user($data1, $id_user);
+        }
+
+    }	
+}
