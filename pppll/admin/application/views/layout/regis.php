a6a8394f4b29d4a536a70f703e3580f40bcc6337
A. Kamal Hidayat <80084933+Eren-Yeagerist@users.noreply.github.com>
2021-07-05 19:02:20 +0700

    Add files via upload

--- pppll/admin/application/views/layout/regis.php
+++ pppll/admin/application/views/layout/regis.php
@@ -5,20 +5,20 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 <!-- Favicon -->
-	<link rel="icon" type="image/png" href="<?php echo base_url('assets/images/icons/favicon.ico') ?>" />
+	<link rel="icon" type="image/png" href="<?php echo base_url('assets/admin/images/icons/favicon.ico') ?>" />
 <!-- Stylesheet -->
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/vendor/bootstrap/css/bootstrap.min.css') ?>" >
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/admin/vendor/bootstrap/css/bootstrap.min.css') ?>" >
 
-	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/fonts/font-awesome-4.7.0/css/font-awesome.min.css') ?>" >
+	<link rel="stylesheet" type="text/css" href="<?php echo base_url('assets/admin/fonts/font-awesome-4.7.0/css/font-awesome.min.css') ?>" >
 
-	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/vendor/animate/animate.css') ?>" >
+	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/admin/vendor/animate/animate.css') ?>" >
 	
-	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/vendor/css-hamburgers/hamburgers.min.css') ?>" >
+	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/admin/vendor/css-hamburgers/hamburgers.min.css') ?>" >
 
-	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/vendor/select2/select2.min.css') ?>" >
+	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/admin/vendor/select2/select2.min.css') ?>" >
 
-	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/css/util.css') ?>" >
-	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/css/main.css') ?>" >
+	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/admin/css/util.css') ?>" >
+	<link rel="stylesheet" type="text/css"href="<?php echo base_url('assets/admin/css/main.css') ?>" >
 
 </head>
 <body>
@@ -27,12 +27,12 @@
 		<div class="container-login100" >
 			<div class="wrap-login100" >
 				<div class="login100-pic js-tilt" data-tilt>
-					<img src="<?php echo base_url('assets/images/img-01.png')?>" alt="IMG">
+					<img src="<?php echo base_url('assets/admin/images/img-01.png')?>" alt="IMG">
 				</div>
 
 				<form class="login100-form validate-form" >
 					<span class="login100-form-title" style="width: 350px;">
-						<!-- Member --> Login
+						<!-- Member --> Register
 					</span>
 
 					<div class="wrap-input100 validate-input" style="width: 350px;" data-validate = "Valid email is required: ex@abc.xyz">
@@ -51,19 +51,41 @@
 							<i class="fa fa-lock" aria-hidden="true"></i>
 						</span>
 					</div>
-					
-					<div class="container-login100-form-btn" style="width: 350px;">
-						<!-- <button class="login100-form-btn" style="background-color:royalblue;">
-							Login
-						</button> -->
-						<a href="<?php echo base_url('index.php/cdashboard'); ?>" class="login100-form-btn" style="background-color:royalblue;">Login</a>
+
+					<div class="wrap-input100 validate-input" style="width: 350px;" data-validate = "Password is required">
+						<input class="input100" type="text" name="alamat_user" placeholder="Alamat User">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
 					</div>
+					<div class="wrap-input100 validate-input" style="width: 350px;" data-validate = "Password is required">
+						<input class="input100" type="text" name="telp_user" placeholder="No Telp User">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" style="width: 350px;" data-validate = "Password is required">
+						<input class="input100" type="text" name="nama_user" placeholder="Nama User">
+						<span class="focus-input100"></span>
+						<span class="symbol-input100">
+							<i class="fa fa-lock" aria-hidden="true"></i>
+						</span>
+					</div>
+					<div class="wrap-input100 validate-input" style="width: 350px;" data-validate = "Password is required">
+						<label>Jenis Kelamin</label><br>
+						<input  type="radio" id="L" name="jk_user" value="Laki-Laki" >
+						<label for="L">Laki-Laki</label><br>
+						<input  type="radio" id="P" name="jk_user" value="Perempuan" >
+						<label for="P">Perempuan</label>
+						
+					</div>
+					
+					
 					<div class="container-login100-form-btn" style="width: 350px;">
 						<button class="login100-form-btn" style="width: 150px;">
 							Register
-						</button>
-						<button class="login100-form-btn" style="width: 200px;background-color:red;">
-							Forgot Password
 						</button>
 					</div>
 
@@ -91,17 +113,17 @@
 
 	
 <!-- Javascript -->
-	<script src="<?php echo base_url('assets/vendor/jquery/jquery-3.2.1.min.js')?>" ></script>
-	<script src="<?php echo base_url('assets/vendor/bootstrap/js/popper.js')?>" ></script>
-	<script src="<?php echo base_url('assets/vendor/bootstrap/js/bootstrap.min.js')?>" ></script>
-	<script src="<?php echo base_url('assets/vendor/select2/select2.min.js')?>" ></script>
-	<script src="<?php echo base_url('assets/vendor/tilt/tilt.jquery.min.js')?>" ></script>
+	<script src="<?php echo base_url('assets/admin/vendor/jquery/jquery-3.2.1.min.js')?>" ></script>
+	<script src="<?php echo base_url('assets/admin/vendor/bootstrap/js/popper.js')?>" ></script>
+	<script src="<?php echo base_url('assets/admin/vendor/bootstrap/js/bootstrap.min.js')?>" ></script>
+	<script src="<?php echo base_url('assets/admin/vendor/select2/select2.min.js')?>" ></script>
+	<script src="<?php echo base_url('assets/admin/vendor/tilt/tilt.jquery.min.js')?>" ></script>
 	<script >
 		$('.js-tilt').tilt({
 			scale: 1.1
 		})
 	</script>
-	<script src="<?php echo base_url('assets/js/main.js')?>" ></script>
+	<script src="<?php echo base_url('assets/admin/js/main.js')?>" ></script>
 
 </body>
 </html>
